2a8774a4067843f281debc6ac6cff7dba0640f21
Yiling-J <njjyl723@gmail.com>
2019-12-11 23:23:19 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+description = 'A memoized/cache decorator for Python using redis.'
+
+
+setup(
+    name="cacheme",
+    url="https://github.com/Yiling-J/cacheme",
+    author="Yiling",
+    author_email="njjyl723@gmail.com",
+    license="BSD-3-Clause",
+    version='v0.1.0',
+    packages=[
+        "cacheme",
+    ],
+    description=description,
+    python_requires=">=3.5",
+    install_requires=[
+        "redis>=3.0.0",
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
