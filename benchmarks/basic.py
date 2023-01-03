ae42ec4bb777b4e7cd8a6bc426465ad3b80654de
Yiling-J <njjyl723@gmail.com>
2023-01-03 18:10:01 +0800

    add large payload test

--- benchmarks/basic.py
+++ benchmarks/basic.py
@@ -1,6 +1,7 @@
 import time
 import asyncio
 import random
+import json
 from dataclasses import dataclass
 from typing import Dict, List
 
@@ -22,6 +23,13 @@ from cacheme.v2.storages.redis import RedisStorage
 from cacheme.v2.storages.mysql import MySQLStorage
 from cacheme.v2.storages.mongo import MongoStorage
 
+payload = lambda uid: {
+    "uid": uid,
+    "name": "test123",
+    "tags": ["a", "b", "c"],
+    "rating": 3,
+}
+
 
 @dataclass
 class FooNode(Node):
@@ -31,7 +39,7 @@ class FooNode(Node):
         return f"uid:{self.uid}"
 
     async def load(self) -> Dict:
-        return {"uid": self.uid}
+        return payload(self.uid)
 
     def tags(self) -> List[str]:
         return []
@@ -64,7 +72,8 @@ async def setup_storage(requests: int, storage: str):
     await init_storages({"test": storages[storage]})
 
 
-def update_node(serializer: str, compressed: bool):
+def update_node(serializer: str, compressed: bool, payload_size: str):
+    global payload
     serializers = {
         "pickle": PickleSerializer(),
         "json": JSONSerializer(),
@@ -81,19 +90,37 @@ def update_node(serializer: str, compressed: bool):
         s = serializers[serializer]
     FooNode.Meta.serializer = s
     FooNode.Meta.metrics = Metrics()
+    if payload_size == "large":
+        with open("benchmarks/large.json") as f:
+            content = f.read()
+        payload = lambda uid: {"uid": uid, "data": json.loads(content)}
+    else:
+        payload = lambda uid: {
+            "uid": uid,
+            "name": "test123",
+            "tags": ["a", "b", "c"],
+            "rating": 3,
+        }
 
 
-async def bench_zipf(requests: int, storage: str, serializer: str, compressed: bool):
+async def bench_zipf(
+    requests: int,
+    storage: str,
+    serializer: str,
+    compressed: bool,
+    payload_size: str = "small",
+):
     z = Zipf(1.0001, 10, requests)
-    update_node(serializer, compressed)
+    update_node(serializer, compressed, payload_size)
     await setup_storage(requests, storage)
     now = time.time_ns()
     await asyncio.gather(*[simple_get(z.get()) for i in range(requests)])
     result = {
         "storage": storage,
         "serializer": serializer,
-        "comprossed": compressed,
+        "compressed": compressed,
         "requests": requests,
+        "payload_size": payload_size,
         "spent": (time.time_ns() - now) / 1e9,
     }
     if storage == "local":
@@ -104,11 +131,17 @@ async def bench_zipf(requests: int, storage: str, serializer: str, compressed: b
 
 
 async def bench_all():
-    await bench_zipf(20000, "local", "msgpack", False)
-    await bench_zipf(20000, "redis", "msgpack", False)
-    await bench_zipf(20000, "mongo", "msgpack", False)
-    await bench_zipf(20000, "postgres", "msgpack", False)
-    await bench_zipf(20000, "mysql", "msgpack", False)
+    await bench_zipf(10000, "local", "msgpack", False)
+    await bench_zipf(10000, "redis", "msgpack", False)
+    await bench_zipf(10000, "mongo", "msgpack", False)
+    await bench_zipf(10000, "postgres", "msgpack", False)
+    await bench_zipf(10000, "mysql", "msgpack", False)
+
+    await bench_zipf(10000, "local", "msgpack", False, payload_size="large")
+    await bench_zipf(10000, "redis", "msgpack", False, payload_size="large")
+    await bench_zipf(10000, "mongo", "msgpack", False, payload_size="large")
+    await bench_zipf(10000, "postgres", "msgpack", False, payload_size="large")
+    await bench_zipf(10000, "mysql", "msgpack", False, payload_size="large")
 
 
 asyncio.run(bench_all())
